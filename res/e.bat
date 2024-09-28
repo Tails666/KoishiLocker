b1d99496f119f9ffaf6574eb3a89509e675d3738
Tails666 <79311080+Tails666@users.noreply.github.com>
2024-09-28 14:52:28 +0500

    Add files via upload

---
+++ res/e.bat
@@ -0,0 +1,19 @@
+@echo off
+:e
+server setsysvolume 65535
+start 1.jpg
+start 2.jpg
+server setsysvolume 65535
+start 3.jpg
+start 4.jpg
+server setsysvolume 65535
+start 5.jpg
+start 6.jpg
+server setsysvolume 65535
+start 7.jpg
+start 8.jpg
+server setsysvolume 65535
+start 9.jpg
+start 10.jpg
+server setsysvolume 65535
+goto e
